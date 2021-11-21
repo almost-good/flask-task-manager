362467cc9a16c4d376e6a59c3fa81c429bab5f4c
unknown <marja.marinic@gmail.com>
2021-11-21 14:05:29 +0000

    Add changes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import (
-    Flask, flash, render_template, 
+    Flask, flash, render_template,
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
@@ -11,21 +11,25 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
-app.config["MONGO_URI"]  = os.environ.get("MONGO_URI")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
-mongo = PyMongo(app)
+
+# IMPORTANT! https://pymongo.readthedocs.io/en/stable/examples/tls.html
+mongo = PyMongo(app, tls=True, tlsAllowInvalidCertificates=True)
 
 
 @app.route("/")
 @app.route("/get_tasks")
 def get_tasks():
+    test = mongo.db.get_collection("tasks").count()
+    print(mongo.db)
     tasks = mongo.db.tasks.find()
     return render_template("tasks.html", tasks=tasks)
 
 
 if __name__ == "__main__":
     app.run(
-        host = os.environ.get("IP"),
-        port = int(os.environ.get("PORT")),
-        debug = True)
+        host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
+        debug=True)
